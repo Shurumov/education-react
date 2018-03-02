544dc7234f4685a851021f14f285d373c6341edf
Aleksei <alexei0113@gmail.com>
2018-03-02 18:29:37 +0700

    create component

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var News = React.createClass({
+    render: function(){
+        return (
+            <div className="news">
+                К сожалению, новостей нет.
+            
+            </div>
+        );
+    }
+});
+
+var Comments  = React.createClass({
+    render: function(){
+        return (
+            <div className="comments">
+                Нет новостей - комментировать нечего.
+            </div>
+        );
+    }
+});
+
+var App = React.createClass({
+    render: function(){
+        return (
+            <div className="app">
+                Всем привет, я компонент App!
+                <News />
+                <Comments />
+            </div>
+            
+        );
+    }
+});
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
