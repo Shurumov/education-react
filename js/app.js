b5a469fb3684c59c0486a9ca5641dea0eac2fed4
shurumov <alexei0113@gmail.com>
2018-03-05 18:12:40 +0700

    start state

--- js/app.js
+++ js/app.js
@@ -1,34 +1,61 @@
 var my_news = [
   {
     author: 'Саша Печкин',
-    text: 'В четверг, четвертого числа...'
+    text: 'В четчерг, четвертого числа...',
+    bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
   },
   {
     author: 'Просто Вася',
-    text: 'Считаю, что $ должен стоить 35 рублей!'
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
   },
   {
     author: 'Гость',
-    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
   }
 ];
 
 var Article = React.createClass({
+    
+    propTypes: {
+        data: React.PropTypes.shape({
+            author: React.PropTypes.string.isRequired,
+            text: React.PropTypes.string.isRequired,
+            bigText: React.PropTypes.string.isRequired
+        })
+    },
+    
+    getInitialState: function() {
+        return {
+            visible: false
+        };
+    },
+    
    render: function(){
        
         var author = this.props.data.author,
-        text = this.props.data.text;
+        text = this.props.data.text,
+        bigText = this.props.data.bigText,
+        visible = this.state.visible;
        
        return(
             <div className="article">
                 <p className="news__author">{author}:</p>
                 <p className="news__text">{text}</p>
+                <a href="#" className={'news__readmore '+ (visible ? 'none' : '')}>Подробнее</a>
+                <p className={'news__big-text ' + visible ? '' : 'none'}>{bigText}</p>
             </div>
        )
    } 
 });
 
 var News = React.createClass({
+    
+    propTypes: {
+        data: React.PropTypes.array.isRequired
+    },
+    
     render: function(){
         
         var data = this.props.data;
@@ -58,16 +85,14 @@ var News = React.createClass({
 
 
 var App = React.createClass({
-    render: function(){
-        return (
-            <div className="app">
-                <h3>Новости</h3>
-                <News data={my_news}/> 
-                
-            </div>
-            
-        );
-    }
+  render: function() {
+    return (
+      <div className='app'>
+        <h3>Новости</h3>
+        <News data={my_news}/>
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(
