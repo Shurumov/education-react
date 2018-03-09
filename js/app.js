336cf0f26d51044b384c71a6c20f387ebf5556c1
shurumov <alexei0113@gmail.com>
2018-03-09 15:49:44 +0700

    add news

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var my_news = [
   {
     author: 'Саша Печкин',
@@ -15,6 +17,8 @@ var my_news = [
     bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
   }
 ];
+
+window.ee = new EventEmitter();
 
 var Article = React.createClass({
   propTypes: {
@@ -89,45 +93,104 @@ var News = React.createClass({
   }
 });
 
-// --- добавили test input ---
-var TestInput = React.createClass({   
-    
-    componentDidMount: function() { //ставим фокус в input
-        ReactDOM.findDOMNode(this.refs.myTestInput).focus();
-    },
-    
-    onBtnClickHandler: function(){
-        alert(ReactDOM.findDOMNode(this.refs.myTestInput).value);
-        console.log(this.refs);
-    },
-    
-    render: function() {
+var Add = React.createClass({
+  getInitialState: function() {
+    return {
+      agreeNotChecked: true,
+      authorIsEmpty: true,
+      textIsEmpty: true
+    };
+  },
+  componentDidMount: function() {
+    ReactDOM.findDOMNode(this.refs.author).focus();
+  },
+  onBtnClickHandler: function(e) {
+    e.preventDefault();
+    var textEl = ReactDOM.findDOMNode(this.refs.text);
+
+    var author = ReactDOM.findDOMNode(this.refs.author).value;
+    var text = textEl.value;
+
+    var item = [{
+      author: author,
+      text: text,
+      bigText: '...'
+    }];
+
+    window.ee.emit('News.add', item);
+
+    textEl.value = '';
+    this.setState({textIsEmpty: true});
+  },
+  onCheckRuleClick: function(e) {
+    this.setState({agreeNotChecked: !this.state.agreeNotChecked});
+  },
+  onFieldChange: function(fieldName, e) {
+    if (e.target.value.trim().length > 0) {
+      this.setState({[''+fieldName]:false})
+    } else {
+      this.setState({[''+fieldName]:true})
+    }
+  },
+  render: function() {
+    var agreeNotChecked = this.state.agreeNotChecked,
+        authorIsEmpty = this.state.authorIsEmpty,
+        textIsEmpty = this.state.textIsEmpty;
     return (
-    <div>
+      <form className='add cf'>
         <input
-          className='test-input'
-          defaultValue=''
-          placeholder='введите значение'
-          ref='myTestInput'
+          type='text'
+          className='add__author'
+          onChange={this.onFieldChange.bind(this, 'authorIsEmpty')}
+          placeholder='Ваше имя'
+          ref='author'
         />
-            
-        <button onClick={this.onBtnClickHandler} ref='alert_button'>Показать alert</button>
-    </div>
+        <textarea
+          className='add__text'
+          onChange={this.onFieldChange.bind(this, 'textIsEmpty')}
+          placeholder='Текст новости'
+          ref='text'
+        ></textarea>
+        <label className='add__checkrule'>
+          <input type='checkbox' ref='checkrule' onChange={this.onCheckRuleClick}/>Я согласен с правилами
+        </label>
+
+        <button
+          className='add__btn'
+          onClick={this.onBtnClickHandler}
+          ref='alert_button'
+          disabled={agreeNotChecked || authorIsEmpty || textIsEmpty}
+          >
+          Опубликовать новость
+        </button>
+      </form>
     );
   }
 });
 
-
-
-
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      news: my_news
+    };
+  },
+  componentDidMount: function() {
+    var self = this;
+    window.ee.addListener('News.add', function(item) {
+      var nextNews = item.concat(self.state.news);
+      self.setState({news: nextNews});
+    });
+  },
+  componentWillUnmount: function() {
+    window.ee.removeListener('News.add');
+  },
   render: function() {
+    console.log('render');
     return (
       <div className='app'>
+        <Add />
         <h3>Новости</h3>
-        <TestInput /> {/* добавили вывод компонента */}
-        
-        <News data={my_news} />
+        <News data={this.state.news} />
       </div>
     );
   }
@@ -136,4 +199,4 @@ var App = React.createClass({
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+);  
